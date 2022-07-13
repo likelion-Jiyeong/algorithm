79cf01f3b0178bcf8256111c38b49c86372283c2
likelion-Jiyeong <julia043130@gmail.com>
2022-07-13 17:07:52 +0900

    add likelion-Jiyeong_p92334.py

--- likelion-Jiyeong_p92334.py
+++ likelion-Jiyeong_p92334.py
@@ -1,10 +1,24 @@
 def solution(id_list, report, k):
-    answer = []
+    answer = [0]*len(id_list)
     map_list = {}
-    for idx, cont in enumerate(report):
-        cont_split = cont.split(' ')
-        map_list[cont_split[0]] = cont_split[1]
-        print(f"map_list >> {map_list}")
-    return answer
+    cnt_list = {} # id별 신고당한 count
+    report = set(report)
+    for i in report:
+        a,b = i.split(' ')
+        
+        if b not in cnt_list: 
+            cnt_list[b] = 1
+        else:
+            cnt_list[b] += 1
 
-solution()
+        if a not in map_list: # 신고한id, 신고당한 id 매핑
+            map_list[a] = [b]
+        elif b not in map_list[a]:
+            map_list[a] += [b]
+     
+    for idx, n in cnt_list.items():
+        if n >= k:
+            for idx2, user2 in map_list.items():
+                if idx in user2:
+                    answer[id_list.index(idx2)] += 1
+    return answer
