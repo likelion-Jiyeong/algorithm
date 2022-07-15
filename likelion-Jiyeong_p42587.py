9b45ca10d62dac369c7398485b0930ed87f8adb9
likelion-Jiyeong <julia043130@gmail.com>
2022-07-15 14:19:34 +0900

    add likelion-Jiyeong_p42587.py

--- likelion-Jiyeong_p42587.py
+++ likelion-Jiyeong_p42587.py
@@ -1,19 +1,13 @@
+from collections import deque
 def solution(prior,loc):
-    # prior 정렬
-    max = 0
-    max_idx = 0
-    wait_arr = []
-    while prior:
-        for idx, val in enumerate(prior):
-            if idx == len(prior):
-                max_idx = idx
-            else:
-                if val > max:
-                    max = val
-                    max_idx = idx
-            prior.pop(max_idx) #prior에서 max_idx번째 요소를 삭제
-            print(f"prior >> {prior}")
-            wait_arr.append(max_idx)
-            print(f"wait_arr >> {wait_arr}")    
-                
-solution([2,1,3,2],2)
+    answer = 0
+    temp = deque(enumerate(prior)) 
+    while temp:
+        ord, prio = temp.popleft()
+        if any(prio < temp_prio[1] for temp_prio in temp): # any 유용함
+            temp.append((ord, prio))
+        else:
+            answer += 1
+            if ord == loc:  # popleft하면서 append시킨 데이터가 돌아옴 
+                break
+    return answer
