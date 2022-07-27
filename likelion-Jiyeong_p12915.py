e5e9a9fcf3306ac65b0c0da261d362e33abf0f21
likelion-Jiyeong <julia043130@gmail.com>
2022-07-27 20:51:14 +0900

    update likelion-Jiyeong_p12915.py

--- likelion-Jiyeong_p12915.py
+++ likelion-Jiyeong_p12915.py
@@ -1,20 +1,21 @@
-def solution(strings, n):
-    answer = []
-    dict = {}
-    temp= [x[n] for x in strings]
-    for idx,val in enumerate(temp):
-        dict[idx] = val
+# 1차 시도 성공했으나 다른 사람들에 비해서 코드가 김
+# def solution(strings, n):
+#     answer = []
+#     dict = {}
+#     strings.sort()
+#     temp= [x[n] for x in strings]
+#     for idx,val in enumerate(temp):
+#         dict[idx] = val
     
-    dict_sort = sorted(dict.items(), key=lambda x: x[1])
-    idxs = [x[0] for x in dict_sort]
-    print(f"idxs >> {idxs}")
-    answer = [strings[x[0]] for x in dict_sort]
+#     dict_sort = sorted(dict.items(), key=lambda x: x[1])
+#     answer = sorted([strings[x[0]] for x in dict_sort], key=lambda x:x[n])
     
-    
-    
-    # for i in dict_sort:
-    #     answer.append(strings[i[0]])
-    print(f"answer >> {answer}")
-    return answer
+#     return answer
 
+# 2줄만에 짤 수 있는 문제였음. 
+def solution(strings, n):
+    strings.sort()
+    answer = sorted(strings, key=lambda x:x[n])
+    print(f"answer >> {answer}")
 solution(["abce", "abcd", "cdx"],2)
+
